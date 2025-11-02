da1bb63d2856f5890db2e576a698993aed7656e0
daniel <contractor@lateralaspect.com.au>
2025-11-02 20:03:24 +0800

    up

---
+++ README.md
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <h1>
+        Give me some credit risk prediction
+    </h1>
+    # uvicorn main:app --host 127.0.0.1 --port 8001
+    # pip install -r requirements.txt
+    # python3 -m http.server
+</html>
--- main.py
+++ main.py
@@ -11,20 +11,19 @@ from pydantic import BaseModel
 from typing import Dict, Union
 
 def engineer_features(df, expected_num_cols=None, expected_cat_cols=None):
-
     df_engi = df.copy()
-    df_engi = df_engi.fillna(0)
+    df_engi_temp = df_engi.fillna(0)
     df_engi["TotalPastDue"] = (
-        df_engi["NumberOfTime30-59DaysPastDueNotWorse"]
-        + df_engi["NumberOfTimes90DaysLate"]
-        + df_engi["NumberOfTime60-89DaysPastDueNotWorse"]
-    )  
+        df_engi_temp["NumberOfTime30-59DaysPastDueNotWorse"]
+        + df_engi_temp["NumberOfTimes90DaysLate"]
+        + df_engi_temp["NumberOfTime60-89DaysPastDueNotWorse"]
+    )
     df_engi["HasDelinquencyBinary"] = (df_engi["TotalPastDue"] > 0).astype(int)
     df_engi["MajorDelinquencyBinary"] = (
-        (df_engi["NumberOfTimes90DaysLate"] > 0)
-        | (df_engi["NumberOfTime60-89DaysPastDueNotWorse"] > 0)
+        (df_engi_temp["NumberOfTimes90DaysLate"] > 0)
+        | (df_engi_temp["NumberOfTime60-89DaysPastDueNotWorse"] > 0)
     ).astype(int)
-    df_engi["IsHighUtilizationBinary"] = (df_engi["RevolvingUtilizationOfUnsecuredLines"] > 0.67).astype(int)
+    df_engi["IsHighUtilizationBinary"] = (df_engi_temp["RevolvingUtilizationOfUnsecuredLines"] > 0.67).astype(int)
     age_notna = df_engi["age"].notna()
     df_engi.loc[age_notna, "AgeBin"] = pd.cut(
         df_engi.loc[age_notna, "age"],
@@ -38,7 +37,6 @@ def engineer_features(df, expected_num_cols=None, expected_cat_cols=None):
     def credit_mix(row):
         if pd.isna(row["NumberRealEstateLoansOrLines"]) or pd.isna(row["NumberOfOpenCreditLinesAndLoans"]):
             return np.nan
-
         if row["NumberRealEstateLoansOrLines"] == 0 and row["NumberOfOpenCreditLinesAndLoans"] == 0:
             return "NoCredit"
         elif row["NumberRealEstateLoansOrLines"] > 0 and row["NumberOfOpenCreditLinesAndLoans"] == 0:
@@ -49,26 +47,27 @@ def engineer_features(df, expected_num_cols=None, expected_cat_cols=None):
             return "MixedCredit"
     df_engi["CreditMix"] = df_engi.apply(credit_mix, axis=1)
     df_engi["IsCreditMixRisky"] = df_engi["CreditMix"].ne("MixedCredit").astype(int)
-    df_engi["HasDebtRatioHigh"] = (df_engi["DebtRatio"] > 0.67).astype(int)
-    df_engi["Has90DaysLate"] = (df_engi["NumberOfTimes90DaysLate"] > 0).astype(int)
+    df_engi["HasDebtRatioHigh"] = (df_engi_temp["DebtRatio"] > 0.67).astype(int)
+    df_engi["Has90DaysLate"] = (df_engi_temp["NumberOfTimes90DaysLate"] > 0).astype(int)
     df_engi["HasAnyLate"] = (
-        df_engi["NumberOfTimes90DaysLate"]
-        + df_engi["NumberOfTime30-59DaysPastDueNotWorse"]
-        + df_engi["NumberOfTime60-89DaysPastDueNotWorse"]
+        df_engi_temp["NumberOfTimes90DaysLate"]
+        + df_engi_temp["NumberOfTime30-59DaysPastDueNotWorse"]
+        + df_engi_temp["NumberOfTime60-89DaysPastDueNotWorse"]
         > 0
     ).astype(int)
     df_engi["HasMultipleLate"] = (
-        df_engi["NumberOfTimes90DaysLate"]
-        + df_engi["NumberOfTime30-59DaysPastDueNotWorse"]
-        + df_engi["NumberOfTime60-89DaysPastDueNotWorse"]
+        df_engi_temp["NumberOfTimes90DaysLate"]
+        + df_engi_temp["NumberOfTime30-59DaysPastDueNotWorse"]
+        + df_engi_temp["NumberOfTime60-89DaysPastDueNotWorse"]
         >= 2
     ).astype(int)
-    df_engi["HasHighOpenCreditLines"] = (df_engi["NumberOfOpenCreditLinesAndLoans"] > 8).astype(int)
+    df_engi["HasHighOpenCreditLines"] = (df_engi_temp["NumberOfOpenCreditLinesAndLoans"] > 8).astype(int)
     df_engi["HasHighDebtLoad"] = (
-        (df_engi["DebtRatio"] > 0.5) & (df_engi["RevolvingUtilizationOfUnsecuredLines"] > 0.67)
+        (df_engi_temp["DebtRatio"] > 0.5) & (df_engi_temp["RevolvingUtilizationOfUnsecuredLines"] > 0.67)
     ).astype(int)
-    df_engi["DebtToIncomeRatio"] = df_engi["DebtRatio"] / (df_engi["MonthlyIncome"] + 1e-3)
-
+    df_engi["DebtToIncomeRatio"] = df_engi_temp["DebtRatio"] / (df_engi_temp["MonthlyIncome"] + 1e-3)
+    for col in ["MonthlyIncome", "NumberOfDependents"]:
+        df_engi[f"{col}_was_missing"] = df[col].isna().astype(int)
     return df_engi
 
 class NN(nn.Module):
@@ -77,10 +76,8 @@ class NN(nn.Module):
         self.emb_layers = nn.ModuleList([nn.Embedding(cat_dim, emb_dim) for cat_dim, emb_dim in zip(cat_dims, emb_dims)])
         self.emb_dropout = nn.Dropout(0.3)
         self.bn_num = nn.BatchNorm1d(num_numeric)
-
         total_emb_dim = sum(emb_dims)
         self.input_dim = num_numeric + total_emb_dim
-
         self.main = nn.Sequential(
             nn.Linear(self.input_dim, 256),
             nn.BatchNorm1d(256),
@@ -95,21 +92,17 @@ class NN(nn.Module):
             nn.ReLU(),
             nn.Dropout(0.1)
         )
-
         self.skip_proj_main = nn.Sequential(
             nn.Linear(self.input_dim, 64),
             nn.Dropout(0.3)
         )
-
         self.cat_skip = nn.Sequential(
             nn.Linear(total_emb_dim, 64),
             nn.BatchNorm1d(64),
             nn.ReLU(),
             nn.Dropout(0.4)
         )
-
         self.out = nn.Linear(64, 1)
-
     def forward(self, x_num, x_cat):
         x_cat_emb = torch.cat([emb(x_cat[:, i]) for i, emb in enumerate(self.emb_layers)], dim=1)
         x_cat_emb = self.emb_dropout(x_cat_emb)
@@ -142,15 +135,13 @@ model.eval()
 
 app = FastAPI(title="Credit Risk Prediction API")
 
-origins = [
-    "*", 
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],  
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
@@ -158,32 +149,23 @@ class InputData(BaseModel):
     data: Dict[str, Union[str, float, int, None]]
 
 def preprocess_input(df: pd.DataFrame):
-
     df_num_raw = df[num_col_order].copy()
     df_num_imputed = pd.DataFrame(num_imputer.transform(df_num_raw), columns=num_col_order)
-
     df_num_scaled = pd.DataFrame(index=df.index)
     if skewed_col_order:
         df_num_scaled[skewed_col_order] = robust_scaler.transform(df_num_imputed[skewed_col_order])
     normal_col_order = [c for c in num_col_order if c not in skewed_col_order]
     if normal_col_order:
         df_num_scaled[normal_col_order] = std_scaler.transform(df_num_imputed[normal_col_order])
-
     x_num_tensor = torch.tensor(df_num_scaled.values, dtype=torch.float32).to(device)
-
     df_cat = df[cat_col_order].copy().astype(str)
-
     for col, rare_cats in rare_maps.items():
         if col in df_cat.columns:
             df_cat[col] = df_cat[col].apply(lambda x: x if x not in rare_cats else 'Other')
-
     df_cat = df_cat.fillna("Other")
-
     for col in cat_col_order:
         df_cat[col] = df_cat[col].map(cat_maps[col]).fillna(0).astype(int)
-
     x_cat_tensor = torch.tensor(df_cat.values, dtype=torch.int64).to(device)
-
     return x_num_tensor, x_cat_tensor
 
 def predict(df: pd.DataFrame, threshold=threshold):
@@ -197,6 +179,6 @@ def predict(df: pd.DataFrame, threshold=threshold):
 
 @app.post("/predict")
 def predict_endpoint(input_data: InputData):
-    df = pd.DataFrame([input_data.data])
+    df = pd.DataFrame([input_data.data]).replace("", np.nan)
     probs, preds = predict(df)
     return {"probabilities": probs.tolist(), "predictions": preds.tolist()}
